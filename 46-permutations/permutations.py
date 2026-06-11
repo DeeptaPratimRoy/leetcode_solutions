fd401cffaa19651a53593166c178a07f7aaf33dc
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-06-11 12:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (85.82%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def permute(self, nums):
+        ans = []
+        visited = [False]*len(nums)
+        def backtrack(subset):
+            if len(subset) == len(nums):
+                ans.append(subset[:])
+                return
+            for i in range(len(nums)):
+                if visited[i] == False:
+                    subset.append(nums[i])
+                    visited[i] = True
+                    backtrack(subset)
+                    subset.pop()
+                    visited[i]=False
+        backtrack([])
+        return ans
+        
