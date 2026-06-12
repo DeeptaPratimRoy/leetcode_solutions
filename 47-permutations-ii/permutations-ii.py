9ced99bd320e95257a3d6d9a754acec8a3f2eefe
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-06-12 12:32:27 +0530

    Time: 15 ms (16.91%) | Memory: 12.9 MB (46.02%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        ans = []
+        visited = [False]*len(nums)
+        def backtrack(subset):
+            if len(subset)==len(nums):
+                ans.append(subset[:])
+                return
+            for i in range(len(nums)):
+                if i > 0:
+                    if nums[i]==nums[i-1] and visited[i-1]==False:
+                        continue
+                if visited[i] == False:
+                    subset.append(nums[i])
+                    visited[i] = True
+                    backtrack(subset)
+                    subset.pop()
+                    visited[i] =False
+        backtrack([])
+        return ans
+
+           
