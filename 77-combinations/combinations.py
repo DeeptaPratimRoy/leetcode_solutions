7edcde7b6f8ab714da004df188b85590845ace37
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-06-15 10:49:37 +0530

    Time: 260 ms (50.94%) | Memory: 131.4 MB (25.35%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def combine(self, n, k):
         ans = []
-        def backtrack(start,subset):
-            if len(subset) == k:
-                ans.append(subset[:])
-                return ans
-            for i in range(start,n+1):
-                subset.append(i)
-                backtrack(i+1,subset)
-                subset.pop()
+        def backtrack(index,current):
+            if len(current) == k:
+                ans.append(current[:])
+                return
+            for i in range(index,n+1):
+                current.append(i)
+                backtrack(i+1,current)
+                current.pop()
         backtrack(1,[])
         return ans
-
-
         
