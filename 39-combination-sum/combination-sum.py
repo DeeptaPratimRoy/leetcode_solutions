d8bf877a0c09eac6a0d6fbe8c5eb6e5c1f6571e7
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-07-07 10:03:17 +0530

    Time: 21 ms (43.67%) | Memory: 12.4 MB (87.06%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
-        ans= []
-        def backtrack(index,curr_sum,subset):
+        ans = []
+        def backtrack(curr_sum,index,subset):
             if curr_sum == target:
                 ans.append(subset[:])
                 return
-            if curr_sum> target:
+            if curr_sum > target:
                 return
             for i in range(index,len(candidates)):
                 subset.append(candidates[i])
-                backtrack(i,curr_sum+candidates[i],subset)
+                backtrack(curr_sum+candidates[i],i,subset)
                 subset.pop()
         backtrack(0,0,[])
         return ans
