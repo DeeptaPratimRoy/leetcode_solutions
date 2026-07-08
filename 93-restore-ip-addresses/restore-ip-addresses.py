9ef186fc5da66ca2d8d20af013945eb4d55e63d3
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-07-08 14:07:35 +0530

    Time: 6 ms (36.73%) | Memory: 12.5 MB (0.65%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -1,22 +1,21 @@
 class Solution(object):
     def restoreIpAddresses(self, s):
         ans = []
-        def backtrack(index,subset):
+        def backtrack(index, subset):
             if len(subset) == 4:
                 if index == len(s):
                     ans.append(".".join(subset))
+                return
+            for length in range(1, 4):
+                if index + length > len(s):
                     return
-            for length in range(1,4):
-                if index+length>len(s):
-                    return
-                substring = s[index:index+length]
-                if len(substring)>1 and substring[0] == "0":
+                substring = s[index:index + length]
+                if len(substring) > 1 and substring[0] == "0":
                     continue
                 if int(substring) > 255:
                     continue
                 subset.append(substring)
-                backtrack(index+length,subset)
+                backtrack(index + length, subset)
                 subset.pop()
-        backtrack(0,[])
+        backtrack(0, [])
         return ans
-        
