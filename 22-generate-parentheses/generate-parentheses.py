10aede097f169de9256c02d85352e8e838395714
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-07-18 13:23:12 +0530

    Time: 2 ms (49.28%) | Memory: 12.6 MB (86.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def generateParenthesis(self, n):
         ans = []
-        def backtrack(open,close,subset):
+        def backtrack(open_count,close_count,subset):
             if len(subset) == 2*n:
                 ans.append("".join(subset))
-            if open < n:    
-                subset.append("(")    
-                backtrack(open+ 1, close,subset)    
-                subset.pop()    
-            if close < open:    
-                subset.append(")")    
-                backtrack(open, close + 1, subset)    
-                subset.pop()    
-        backtrack(0, 0, [])    
+                return
+            if open_count<n:
+                subset.append("(")
+                backtrack(open_count+1,close_count,subset)
+                subset.pop()
+            if close_count < open_count:
+                subset.append(")")
+                backtrack(open_count,close_count+1,subset)
+                subset.pop()
+        backtrack(0,0,[])
         return ans
+
 
         
