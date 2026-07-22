1344cf8822f070de04d24354495a07e51572dd2c
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-07-22 22:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (3.68%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def preorderTraversal(self, root):
+        ans = []
+        def dfs(node):
+            if not node:
+                return
+            ans.append(node.val)   
+            dfs(node.left)         
+            dfs(node.right)        
+        dfs(root)
+        return ans
