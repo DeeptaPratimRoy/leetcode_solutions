f50970d971de03b622727228e2d507ee4a03e900
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-08-23 20:30:54 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (6.09%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -3,10 +3,7 @@ class Solution(object):
         def bst(node, min_value, max_value):
             if node is None:
                 return True
-
             if node.val <= min_value or node.val >= max_value:
                 return False
-
             return bst(node.left, min_value, node.val) and bst(node.right, node.val, max_value)
-
         return bst(root, float('-inf'), float('inf'))
