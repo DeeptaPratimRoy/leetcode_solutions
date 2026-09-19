d360008eb8a9271decb8def914b7ed9c12bf1db7
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-09-19 13:23:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.57%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -1,28 +1,30 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
+
 class Solution:
     def isCousins(self, root: TreeNode | None, x: int, y: int) -> bool:
-        queue = deque([(root,None)])
+
+        queue = deque([(root, None)])
+
         while queue:
             n = len(queue)
-            px = py =None
+            px = py = None
+
             for i in range(n):
-                node,parent = queue.popleft()
+                node, parent = queue.popleft()
+
                 if node.val == x:
                     px = parent
+
                 if node.val == y:
                     py = parent
+
                 if node.left:
-                    queue.append((node.left,node))
+                    queue.append((node.left, node))
+
                 if node.right:
-                    queue.append((node.right,node))
-            if px is not None or py is not None:
-                if px is not None and py is not None and px!=py:
-                    return True
-                else:
-                    return False
+                    queue.append((node.right, node))
+
+            if px is not None and py is not None and px != py:
+                return True
+
         return False
