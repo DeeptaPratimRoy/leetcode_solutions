95ae2faed19709fb52e308866dcb3ea601d1bd5c
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-09-20 12:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (34.27%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,19 +1,18 @@
 from collections import deque
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
+    def rightSideView(self, root):
+        if not root:
             return []
-        queue = deque()
-        queue.append(root)
+        queue = deque([root])
         result = []
         while queue:
-            level = len(queue)
-            for i in range(level):
+            n = len(queue)
+            for i in range(n):
                 node = queue.popleft()
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-                if i == level-1:
+                if i == n - 1:
                     result.append(node.val)
         return result
