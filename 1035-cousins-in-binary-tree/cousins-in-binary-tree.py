abbcd509814e615511641c0d17b01e0225010938
DeeptaPratimRoy <deeptapratimroy@gmail.com>
2026-09-24 20:41:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (97.63%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -3,20 +3,20 @@ class Solution:
     def isCousins(self, root: TreeNode | None, x: int, y: int) -> bool:
         if root is None:
             return False
-        queue = deque([(root, None)])
+        queue = deque([(root,None)])
         while queue:
-            n = len(queue)
             px = py = None
+            n= len(queue)
             for i in range(n):
-                node, parent = queue.popleft()
+                node,parent = queue.popleft()
                 if node.val == x:
                     px = parent
                 if node.val == y:
                     py = parent
                 if node.left:
-                    queue.append((node.left, node))
+                    queue.append((node.left,node))
                 if node.right:
-                    queue.append((node.right, node))
+                    queue.append((node.right,node))
             if px is not None and py is not None and px != py:
                 return True
         return False
